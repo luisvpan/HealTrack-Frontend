6c55bb313a83c4b8fccfcf14f73504519b7215d3
luisvpan <161976502+luisvpan@users.noreply.github.com>
2024-06-23 19:34:00 -0400

    fix

---
+++ src/services/auth/register.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+// Own
+import { API_BASE_URL } from "config/constants";
+import { AllRole } from "core/users/types";
+import BackendError from "exceptions/backend-error";
+
+const URL = `${API_BASE_URL}/auth/register`;
+
+export default async function login(body: LoginBody): Promise<LoginResponse> {
+  try {
+    const response = await axios.post<LoginResponse>(URL, body);
+    return response.data;
+  } catch (error: unknown) {
+    throw new BackendError(error);
+  }
+}
+
+export interface LoginBody {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  id: number;
+  employee?: number;
+  patient?: number;
+  name: string;
+  email: string;
+  token: string;
+  role: AllRole;
+}
