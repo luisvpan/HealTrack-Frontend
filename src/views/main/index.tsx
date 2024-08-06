84db7a0ff3a336280213ce1a270105185395b60a
César <cesarsotillo16@gmail.com>
2024-08-06 09:14:18 -0400

    cambios

--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import { Box, Stack, Typography } from "@mui/material";
+import { useAppSelector } from "store";
 
 const MainPage: React.FC = () => {
+  const user = useAppSelector((state) => state.auth.user);
+
   return (
-    <div>
-      <h1>Bienvenido a la Aplicación</h1>
-      <p>Esta es la página principal de la aplicación donde encontrarás información relevante.</p>
-    </div>
+    <Box sx={{ p: 2 }}>
+      <Stack spacing={2}>
+        <Typography variant="h3">Bienvenido a la aplicación, {user?.name}</Typography>
+        <Typography variant="body1">
+          Esta es la página principal de la aplicación donde encontrarás información relevante.
+        </Typography>
+      </Stack>
+    </Box>
   );
 };
 
