eab976fb0e2a095581c8b3e5b7aafb7169d3a609
César <cesarsotillo16@gmail.com>
2024-08-30 22:56:28 -0400

    feat: Reports Banner in Main page

--- src/views/chat/index.tsx
+++ src/views/chat/index.tsx
@@ -1,11 +1,20 @@
 import { FormEvent, useCallback, useEffect, useRef, useState } from "react";
-import { Box, TextField, Button, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  TextField,
+  Button,
+  Paper,
+  Typography,
+  IconButton,
+  Modal,
+} from "@mui/material";
 import ChatMessage from "./ChatMessage";
 import { ChatInfo, Message } from "core/chats/types";
 import store from "store";
-
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
 import getMessagesById from "services/messages/get-messages-by-id.service";
 import sendMessage from "services/messages/create-message.service";
+import sendImage from "services/messages/upload-chat-image.service"; // Import the sendImage function
 import { useNavigate, useParams } from "react-router-dom";
 import { Socket, io } from "socket.io-client";
 import MainCard from "components/cards/MainCard";
@@ -18,6 +27,8 @@ const Chat = () => {
   const [messageToSend, setMessageToSend] = useState<string>("");
   const [messages, setMessages] = useState<Message[]>([]);
   const [chatInfo, setChatInfo] = useState<ChatInfo | null>(null);
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   const { chatId } = useParams<{ chatId: string }>();
 
@@ -81,6 +92,23 @@ const Chat = () => {
     },
     [chatId, messageToSend, user]
   );
+
+  const handleImageUpload = async () => {
+    if (imageFile) {
+      try {
+        await sendImage(imageFile, Number(chatId));
+        setImageFile(null);
+        setIsModalOpen(false);
+
+        socket.current!.emit("send_message", {
+          user,
+          message: { message: "Image sent" },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   useEffect(() => {
     socket.current!.on("connect", () => {
@@ -177,6 +205,14 @@ const Chat = () => {
 
         <Box component="form" onSubmit={submitMessage}>
           <Paper sx={{ display: "flex", p: 2 }}>
+            <IconButton
+              color="primary"
+              component="label"
+              onClick={() => setIsModalOpen(true)}
+            >
+              <PhotoCameraIcon />
+            </IconButton>
+
             <TextField
               fullWidth
               placeholder="Escribe un mensaje..."
@@ -193,6 +229,55 @@ const Chat = () => {
           </Paper>
         </Box>
       </Box>
+
+      <Modal
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            borderRadius: 1,
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Cargar Imagen
+          </Typography>
+          <input
+            type="file"
+            accept="image/png, image/jpeg"
+            onChange={(e) => {
+              if (e.target.files) setImageFile(e.target.files[0]);
+            }}
+          />
+          <Box mt={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleImageUpload}
+            >
+              Enviar
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={() => setIsModalOpen(false)}
+              sx={{ ml: 2 }}
+            >
+              Cerrar
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
     </MainCard>
   );
 };
--- src/views/main/index.tsx
+++ src/views/main/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { AllRole } from "core/users/types";
 import { Box, Stack, Typography } from "@mui/material";
 import { useAppSelector } from "store";
@@ -10,12 +11,15 @@ import { Employee } from "core/employees/types";
 import { Patient } from "core/patients/types";
 import PatientDetail from "./detail";
 import EmployeeDetail from "./employeeDetail";
+import useGetWarningInfo from "./warningInfo/use-get-warning-info";
+import WarningBanner from "./warningInfo/warning-banner";
 
 const MainPage: React.FC = () => {
   const user = useAppSelector((state) => state.auth.user);
   const [employees, setEmployees] = useState<Employee[]>([]);
   const [patients, setPatients] = useState<Patient[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const { isLoading: warningLoading, warning } = useGetWarningInfo();
 
   useEffect(() => {
     if (user?.role !== AllRole.PATIENT) {
@@ -46,30 +50,32 @@ const MainPage: React.FC = () => {
   return (
     <Box sx={{ p: 2 }}>
       <Stack spacing={2}>
-        <Typography variant="h1" sx={{textAlign: { xs:"center", sm:"start" }}}>
+        <Typography variant="h1" sx={{ textAlign: { xs: "center", sm: "start" } }}>
           Bienvenido a la aplicación HealTrack, {user?.name}
         </Typography>
+        
+        { user?.role === AllRole.PATIENT ? (
+          <WarningBanner warning={warning} isLoading={warningLoading} />
+        ) : null}
+
         {loading ? (
           <Typography variant="body1">Cargando datos...</Typography>
         ) : user?.role === AllRole.PATIENT ? (
           <PatientDetail />
         ) : user?.role === AllRole.ADMIN ? (
           <>
-            <Typography variant="body1" sx={{textAlign: { xs:"center", sm:"start" }}}>
+            <Typography variant="body1" sx={{ textAlign: { xs: "center", sm: "start" } }}>
               Esta es la página principal de la aplicación donde encontrarás información relevante.
             </Typography>
             <EmployeeDetail />
-            <Typography variant="h3">
-              Ultimos 10 empleados en ser registrados:  
-            </Typography>
+            <Typography variant="h3">Últimos 10 empleados en ser registrados:</Typography>
             <TableNewestEmployees items={employees} />
-            <Typography variant="h3"></Typography>
-            <Typography variant="h3">Ultimos 10 pacientes en ser registrados:</Typography>
+            <Typography variant="h3">Últimos 10 pacientes en ser registrados:</Typography>
             <TableNewestPatients items={patients} />
           </>
         ) : (
           <>
-            <Typography variant="body1" sx={{textAlign: { xs:"center", sm:"start" }}}>
+            <Typography variant="body1" sx={{ textAlign: { xs: "center", sm: "start" } }}>
               Esta es la página principal de la aplicación donde encontrarás información relevante.
             </Typography>
             <EmployeeDetail />
